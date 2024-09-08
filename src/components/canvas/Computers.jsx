e37e2ee6234fed10bbb8938fe855db900da5a0f8
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2024-09-08 20:58:27 +0530

    commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
+import canvasLoader from "../Loader";
+
+const Computers = ({isMobile}) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf') 
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={2.25} groundColor="black"/>
+      <pointLight position={[-0.6,-0.7,-0.5]} intensity={2} />
+      <spotLight
+        position={[10, 15, 10]}  // Move the light closer
+        angle={0.3}  // Widen the angle
+        penumbra={1}
+        intensity={2}
+        castShadow
+        shadow-mapSize={1024}
+      />
+
+      <primitive
+        object={computer.scene}
+        scale={ isMobile? 0.7 : 0.75}
+        position={isMobile? [0,-2.5,-2.2] :[0, -4.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+        castShadow
+        receiveShadow
+      />
+
+    </mesh>
   )
 }
+const ComputersCanvas = () => {
 
-export default Computers
+  const [isMobile,setIsMobile] = useState(false);
+
+  useEffect(()=>{
+
+    const mediaQuery = window.matchMedia('(max-width:500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    mediaQuery.addEventListener('change',handleMediaQueryChange);
+
+    return ()=> {
+mediaQuery.removeEventListener('change',handleMediaQueryChange)
+    }
+  })
+  return(
+    <Canvas
+    frameloop='demand'
+    shadows
+    camera={{position:[20,3,5], fov: 25}}
+    gl = {{preserveDrawingBuffer:true}}>
+     
+     <Suspense>
+      <OrbitControls 
+      enableZoom={false}
+      maxPolarAngle={Math.PI / 2} 
+      minPolarAngle={Math.PI / 2} 
+      />
+      <Computers isMobile = {isMobile} />
+     </Suspense>
+     <Preload all />
+    </Canvas>
+  )
+}
+export default ComputersCanvas
