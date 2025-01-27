6c3c557c8d8e335810e2f2731d0d4d2616c3fcf1
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2025-01-27 17:57:44 +0530

    social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom"
 import {About , Contact, Experience , Hero,Navbar,Tech,Works,StarsCanvas} from './components'
+import Social from "./components/Social"
 const App = () => {
   return (
    <BrowserRouter>
     <div className="relative z-0 bg-primary">
+      <Social />
        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center ">
           <Navbar />
           <Hero />
