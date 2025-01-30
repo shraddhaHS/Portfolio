897c3220a4ca7a1eee887bbb9eb37f7fe91a3b59
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2025-01-30 13:38:05 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Shraddha</span>
+            Hi, I'm <span className='text-[#915EFF]'>Shradha</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop softwares , user <br className='sm:block hidden' />
