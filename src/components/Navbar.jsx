85102b7a9986932bba67592f7dda2895f8f59492
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2025-01-30 13:40:39 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Shraddha&nbsp;<span className='sm:block hidden'>| Bhadoria</span> </p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Shradha&nbsp;<span className='sm:block hidden'>| Bhadoria</span> </p>
   
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10 '>
